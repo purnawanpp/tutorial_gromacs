417fb42a11a28a125b075bb75c8db7d578a0773c
Purnawan Pontana Putra <77323253+purnawanpp@users.noreply.github.com>
2024-10-15 13:22:46 +0700

    Update WAWAN-Analysis-GMX.sh

--- WAWAN-Analysis-GMX.sh
+++ WAWAN-Analysis-GMX.sh
@@ -1,49 +1,72 @@
-#! /bin/bash
+#!/bin/bash
 
-echo "Center the trajectory and remove PBC artifacts choose 1 protein and 0 sistem."
+# Warning about the first step
+echo "Center the trajectory and remove PBC artifacts, select 1 for protein and 0 for the system."
 
-# Step 1: Center the trajectory and remove PBC artifacts choose 1 protein dan 0 sistem
+# Step 1: Center the trajectory and remove PBC artifacts
 gmx trjconv -s md.tpr -f md.xtc -o md_center.xtc -n index.ndx -center -pbc mol -ur compact
-echo "RMSD analysis, choose backbone 4 and ligand 13 analysis, this depends on what will be analyzed later."
 
-# Step 2: RMSD analysis, Select backbone analysis 4 and ligand 13, this depends on what will be analyzed later
+echo "RMSD analysis, select 4 for backbone and 13 for ligand, depending on the analysis being performed."
+
+# Step 2: RMSD analysis
 gmx rms -s md.tpr -f md_center.xtc -o rmsd_pro.xvg -tu ns
+export LC_NUMERIC="en_US.UTF-8"
 
-# Step 3: Plot RMSD using xmgrace 
+# Step 3: Plot RMSD using xmgrace
 xmgrace rmsd_pro.xvg
-echo "RMSF analysis, choose c-alfa 3, this depends on what will be analyzed later."
 
-# Step 4: RMSF analysis, choose c-alfa 3, this is flexible according to what will be analyzed
+echo "RMSF analysis, select c-alpha (3), depending on the desired analysis."
+
+# Step 4: RMSF analysis
 gmx rmsf -f md_center.xtc -s md.tpr -o rmsf.xvg -res
 
 # Step 5: Plot RMSF using xmgrace
 xmgrace rmsf.xvg
-echo "SASA (Solvent Accessible Surface Area) analysis, choose protein 1 or depending on what you want it can be backbone, or c alpha."
 
-# Step 6: SASA (Solvent Accessible Surface Area) analysis, Choose protein 1 or flexible as desired, bacbone or c alpha
+echo "SASA (Solvent Accessible Surface Area) analysis, select protein (1) or depending on the desired analysis."
+
+# Step 6: SASA analysis
 gmx sasa -s md.tpr -f md_center.xtc -o sasa.xvg -tu ns
 
 # Step 7: Plot SASA using xmgrace
 xmgrace sasa.xvg
-echo "Radius of gyration analysis, choose main chain 5, this is flexible too."
 
-# Step 8: Radius of gyration analysis, Choose main chain 5, it's flexible too
+echo "Radius of gyration analysis, select main chain (5), this is flexible too."
+
+# Step 8: Radius of gyration analysis
 gmx gyrate -s md.tpr -f md_center.xtc -o gyration.xvg
 
+# Step 9: Modify the first column of gyration.xvg (divide by 1000)
+if [ -f "gyration.xvg" ]; then
+    awk '
+    /^#/ || /^@/ {print $0; next}
+    {
+        $1 = $1 / 1000
+        printf "%.6f ", $1
+        for (i = 2; i <= NF; i++) {
+            printf "%s ", $i
+        }
+        print ""
+    }
+    ' "gyration.xvg" > modified_gyration.xvg
+    echo "The first column of gyration.xvg has been modified and saved as modified_gyration.xvg."
+else
+    echo "gyration.xvg file not found!"
+fi
 
-# Step 9: Plot gyration using xmgrace
-xmgrace gyration.xvg
-echo "Hydrogen bond analysis, hydrogen bond for protein 1 and ligand 13 analysis."
+# Step 10: Plot the modified gyration using xmgrace
+xmgrace modified_gyration.xvg
 
+echo "Hydrogen bond analysis, select protein (1) and ligand (13)."
 
-# Step 10: Hydrogen bond analysis, hydrogen bond for protein 1 and ligand 13 analysis
+# Step 11: Hydrogen bond analysis
 gmx hbond -s md.tpr -f md_center.xtc -num hbond.xvg -tu ns 
 
-
-# Step 11: Plot hydrogen bond data using xmgrace
+# Step 12: Plot hydrogen bond data using xmgrace
 xmgrace hbond.xvg
 
 # End message and script credits
 echo "Analysis completed successfully."
 echo "Script written by Purnawan Pontana Putra, Email: purnawanpp@phar.unand.ac.id"
+echo "Please cite this code as: Putra PP, Indradi RB, Yuniarta TA, Hanifa D, Syaban MFR, Suharti N. Computational investigation of Pluchea indica mechanism targeting peroxisome proliferator-activated receptor gamma. J Herbmed Pharmacol. 2024;13(4):630-639. doi: 10.34172/jhp.2024.52544."
 
